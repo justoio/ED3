70bae6421254304217fd8a79ff35cbfc3bf5805d
justoio <51836147+justoio@users.noreply.github.com>
2019-10-10 06:47:34 -0300

    Remake

--- dma.c
+++ dma.c
@@ -1,22 +1,15 @@
 /*
 ===============================================================================
  Name        : FinalProject.c
- Authors      : $(author)
- Version     :
+ Author      : @justoio
+ Version     : 
  Copyright   : $(copyright)
  Description : main definition
 ===============================================================================
 */
-
 #include "LPC17xx.h"
-#include "lpc17xx_timer.h"
-#include "lpc17xx_gpio.h"
 #include "lpc17xx_gpdma.h"
 #include "lpc17xx_dac.h"
-#include "lpc17xx_adc.h"
-#include "lpc17xx_uart.h"
-#include "lpc17xx_pinsel.h"
-/* Directives ------------------------------- */
 #define TRUE 1
 #define FALSE 0
 #define LOW	0
@@ -24,17 +17,10 @@
 #define PIN22 (uint32_t)(1<<22)
 #define PORT0 (uint8_t)((0))
 #define OUTPUT (uint8_t)((1))
-void setConfigs();
-/* Confgiruacion Perifericos ----------------------- */
-void configTIMERS();
-void configGPIO();
 void configDMA(uint32_t*);
 void configDAC();
-void configADC();
-void configUART();
 void configPIN();
-/* GPIO style ------------------------------- */
-void UART_IntReceive();
+void loadTABLA();
 /*********************************************************************
  * @brief
  * @param[in]     None
@@ -44,67 +30,14 @@ void UART_IntReceive();
  **********************************************************************/
 int main(){
 	uint32_t TABLA_DAC[60];
-	uint32_t i;
-	for(i=0;i<NUMERO_MUESTRAS;i++)
-		{
-			if(i<=15)	//De 0 a 90º
-			{
-				TABLA_DAC[i] = 0;
-				//if(i==15) TABLA_DAC[i]= 1023;
-			}
-			else if(i<=30)	//De 90º a 180º
-			{
-				TABLA_DAC[i] = 500;
-			}
-			else if(i<=45)	//De 180º a 270º
-			{
-				TABLA_DAC[i] = 750;
-			}
-			else	//De 270º a 360º
-			{
-				TABLA_DAC[i] = 1000;
-			}
-			TABLA_DAC[i] = (TABLA_DAC[i]<<6);
-	}
-	
+	loadTABLA(TABLA_DAC);
+	configPIN();
 	configDAC();//tmp
-	configDMA(TABLA_DAC);//tmp
+	configDMA(TABLA_DAC);
 	while(TRUE){
 
 	}
-    return FALSE;}
-void setConfigs(){
-	//configTIMERS();
-	//configGPIO();
-	//configDMA();
-	//configDAC();
-	//configADC();
-	//configUART();
-	//configPIN();
-	return;}
-/*********************************************************************
- * @brief
- * @param[in]     None
- * @return        None
- *
- * Note:
- **********************************************************************/
-void configTIMERS(){
-    TIM_TIMERCFG_Type struct_timer;
-    TIM_MATCHCFG_Type struct_match;
-    struct_timer.PrescaleOption=TIM_PRESCALE_USVAL;
-    struct_timer.PrescaleValue=100;
-    struct_match.MatchChannel=0;
-    struct_match.IntOnMatch=ENABLE;
-    struct_match.StopOnMatch=DISABLE;//Que no se detenga luego
-    struct_match.ResetOnMatch=ENABLE;
-    struct_match.ExtMatchOutputType=TIM_EXTMATCH_NOTHING;
-    struct_match.MatchValue=10000;
-    TIM_Init(LPC_TIM0,TIM_TIMER_MODE,&struct_timer);
-    TIM_ConfigMatch(LPC_TIM0,&struct_match);
-    TIM_Cmd(LPC_TIM0,ENABLE);
-    NVIC_EnableIRQ(TIMER0_IRQn);
-    return;}
+return FALSE;}
 /*********************************************************************
  * @brief
  * @param[in]     None
@@ -135,7 +68,7 @@ void configDMA(uint32_t* tmptestDMA){
     struct_ch_dma.DMALLI=(uint32_t)&struct_lli_dma;
     GPDMA_Setup(&struct_ch_dma);
     GPDMA_ChannelCmd(0,ENABLE);//Activo el Canal 0
-    return;}
+return;}
 /*********************************************************************
  * @brief
  * @param[in]     None
@@ -143,27 +76,13 @@ void configDMA(uint32_t* tmptestDMA){
  *
  * Note:
  **********************************************************************/
-void configUART(){
-	UART_CFG_Type UARTConfigStruct;
-	UART_FIFO_CFG_Type UARTFIFOConfigStruct;
-	UART_ConfigStructInit(&UARTConfigStruct);
-	UART_Init(LPC_UART0, &UARTConfigStruct);
-	UART_FIFOConfigStructInit(&UARTFIFOConfigStruct);
-	UART_FIFOConfig(LPC_UART0, &UARTFIFOConfigStruct);
-	UART_IntConfig(LPC_UART0, UART_INTCFG_RBR, ENABLE);
-	UART_IntConfig(LPC_UART0, UART_INTCFG_RLS, ENABLE);
-	NVIC_EnableIRQ(UART0_IRQn);
-	return;}
-/*********************************************************************
- * @brief
- * @param[in]     None
- * @return        None
- *
- * Note:
- **********************************************************************/
-void configGPIO(){
-	GPIO_SetDir(PORT0,PIN22,OUTPUT);
-	return;}
+void configDAC(){
+	DAC_CONVERTER_CFG_Type DAC_Struct;
+	DAC_Struct.CNT_ENA =SET;		//Activa el modo timeout
+	DAC_Struct.DMA_ENA = SET;		//Activa el modo
+	DAC_Init(LPC_DAC);
+	DAC_ConfigDAConverterControl(LPC_DAC, &DAC_Struct);	
+return;}
 /*********************************************************************
  * @brief
  * @param[in]     None
@@ -173,12 +92,6 @@ void configGPIO(){
  **********************************************************************/
 void configPIN(){
 	PINSEL_CFG_Type pinsel_cfg;
-	pinsel_cfg.Portnum=PINSEL_PORT_0;
-	pinsel_cfg.Pinnum=PINSEL_PIN_22;
-	pinsel_cfg.Funcnum=PINSEL_FUNC_0;
-	pinsel_cfg.Pinmode=PINSEL_PINMODE_PULLUP;
-	pinsel_cfg.OpenDrain=PINSEL_PINMODE_NORMAL;
-	PINSEL_ConfigPin(&pinsel_cfg);
 	pinsel_cfg.Portnum=PINSEL_PORT_0;
 	pinsel_cfg.Pinnum=PINSEL_PIN_26;
 	pinsel_cfg.Funcnum=PINSEL_FUNC_2;//?
@@ -193,74 +106,27 @@ void configPIN(){
  *
  * Note:
  **********************************************************************/
-void configDAC(){
-	DAC_CONVERTER_CFG_Type DAC_Struct;
-	DAC_Struct.CNT_ENA =SET;		//Activa el modo timeout
-	DAC_Struct.DMA_ENA = SET;		//Activa el modo
-	DAC_Init(LPC_DAC);
-	DAC_ConfigDAConverterControl(LPC_DAC, &DAC_Struct);	
-	return;}
-void configADC(){return;}
-
-void UART0_IRQHandler(void){
-		uint32_t intsrc, tmp, tmp1;
-		//Determina la fuente de interrupción
-		intsrc = UART_GetIntId(LPC_UART0);
-		tmp = intsrc & UART_IIR_INTID_MASK;
-		// Evalúa Line Status
-		if (tmp == UART_IIR_INTID_RLS){
-				tmp1 = UART_GetLineStatus(LPC_UART0);
-				tmp1 &= (UART_LSR_OE | UART_LSR_PE | UART_LSR_FE  | UART_LSR_BI | UART_LSR_RXFE);
-				// ingresa a un Loop infinito si hay error
-				if (tmp1) {
-						while(1){};
-				}
-		}
-		// Receive Data Available or Character time-out
-		if ((tmp == UART_IIR_INTID_RDA) || (tmp == UART_IIR_INTID_CTI)){
-				UART_IntReceive();
-
-		}
-		return;}
-/*********************************************************************
- * @brief
- * @param[in]     None
- * @return        None
- *
- * Note:
- **********************************************************************/
-void TIMER0_IRQHandler(void){
-	TIM_ClearIntPending(LPC_TIM0,TIM_MR0_INT);
-	uint32_t tmp_port0=GPIO_ReadValue(PORT0);
-	if(tmp_port0&PIN22)
-		GPIO_ClearValue(PORT0,PIN22);
-	else
-		GPIO_SetValue(PORT0,PIN22);
-	return;
-}
-/*********************************************************************
- * @brief
- * @param[in]     None
- * @return        None
- *
- * Note:
- **********************************************************************/
-void UART_IntReceive(void){
-	/*
-	UART_Receive(LPC_UART0, info, sizeof(info), NONE_BLOCKING);
-	return;
-	*/
-	return;
-}
-/*********************************************************************
- * @brief
- * @param[in]     None
- * @return        None
- *
- * Note:
- **********************************************************************/
-void testTimer(void){
-	configGPIO();
-	configTIMERS();
-	return;
-}
+void loadTABLA(uint32_t* TABLA_DAC){
+	uint32_t i;
+	for(i=0;i<NUMERO_MUESTRAS;i++)
+		{
+			if(i<=15)	//De 0 a 90º
+			{
+				TABLA_DAC[i] = 0;
+				//if(i==15) TABLA_DAC[i]= 1023;
+			}
+			else if(i<=30)	//De 90º a 180º
+			{
+				TABLA_DAC[i] = 500;
+			}
+			else if(i<=45)	//De 180º a 270º
+			{
+				TABLA_DAC[i] = 750;
+			}
+			else	//De 270º a 360º
+			{
+				TABLA_DAC[i] = 1000;
+			}
+			TABLA_DAC[i] = (TABLA_DAC[i]<<6);
+	}	
+return;}
