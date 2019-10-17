2dc730cf775446416009d869dfc52378ca4825c1
justoio <51836147+justoio@users.noreply.github.com>
2019-10-17 16:24:51 -0300

    Update spi_test.c

--- spi_test.c
+++ spi_test.c
@@ -10,7 +10,7 @@
 #define BUFFER_SIZE 0x40
 void confPin(void);
 void confSPI(void);
-void transmitir(uint16_t*,uint16_t*,uint32_t);
+void readWrite(uint16_t*,uint16_t*,uint32_t);
 int main (void){
 	uint16_t Tx_Buf[BUFFER_SIZE];
 	uint16_t Rx_Buf[BUFFER_SIZE];
@@ -20,7 +20,7 @@ int main (void){
 		Tx_Buf[i] = i;
 		Rx_Buf[i] = 0;
 	}
-	transmitir(Tx_Buf,Rx_Buf,0);
+	readWrite(Tx_Buf,Rx_Buf,0);
 	while(1){}
 return 0;}
 void confPin(void){
@@ -48,20 +48,20 @@ void confPin(void){
 	return;}
 void confSPI(void){
 	SPI_CFG_Type SPI_ConfigStruct;
-	SPI_ConfigStruct.CPHA = SPI_CPHA_SECOND;
-	SPI_ConfigStruct.CPOL = SPI_CPOL_LO;
-	SPI_ConfigStruct.ClockRate = 2000000;
+	SPI_ConfigStruct.CPHA = SPI_CPHA_SECOND;//CPHA=1
+	SPI_ConfigStruct.CPOL = SPI_CPOL_LO;// CPOL=0
+	SPI_ConfigStruct.ClockRate = 2000000;//2Mhz
 	SPI_ConfigStruct.DataOrder = SPI_DATA_MSB_FIRST;
-	SPI_ConfigStruct.Databit = SPI_DATABIT_SIZE;
+	SPI_ConfigStruct.Databit = SPI_DATABIT_SIZE;//10?
 	SPI_ConfigStruct.Mode = SPI_MASTER_MODE;
 	// Initialize SPI peripheral with parameter given in structure above
 	SPI_Init(LPC_SPI, &SPI_ConfigStruct);
 return;}
-void transmitir(uint16_t* Tx_Buf,uint16_t* Rx_Buf,uint32_t len){
+void readWrite(uint16_t* Tx_Buf,uint16_t* Rx_Buf,uint32_t len){
 	SPI_DATA_SETUP_Type xferConfig;
 	xferConfig.tx_data = Tx_Buf;
 	xferConfig.rx_data = Rx_Buf;
 	xferConfig.length = BUFFER_SIZE;
-	len = SPI_ReadWrite(LPC_SPI, &xferConfig, SPI_TRANSFER_POLLING);
+	len = SPI_ReadWrite(LPC_SPI, &xferConfig, SPI_TRANSFER_POLLING);//Returns 64
 	SPI_DeInit(LPC_SPI);
 return;}
